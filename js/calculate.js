2cf6a0cedc816fae6d9e69b102aff983f2c8d6c5
Khen <palerkhen40@gmail.com>
2023-03-26 21:03:37 +0800

    grading system -done

---
+++ js/calculate.js
@@ -0,0 +1,45 @@
+function calculateGrade() 
+{
+  // Get the subject inputs from the user
+  const subject1 = parseInt(document.getElementById("subject1").value);
+  const subject2 = parseInt(document.getElementById("subject2").value);
+  const subject3 = parseInt(document.getElementById("subject3").value);
+  const subject4 = parseInt(document.getElementById("subject4").value);
+  const subject5 = parseInt(document.getElementById("subject5").value);
+  const subject6 = parseInt(document.getElementById("subject6").value);
+  const subject7 = parseInt(document.getElementById("subject7").value);
+
+  // Calculate the average grade
+  let averageGrade = (subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7) / 7;
+
+  // Determine the descriptor and remarks based on the average grade
+  let descriptor = "";
+  let remarks = "";
+  if (averageGrade > 100) {
+    descriptor = "DOES NOT EXIST";
+    remarks = "DOES NOT EXIST";
+  }else if (averageGrade >= 90 && averageGrade <= 100) {
+    descriptor = "OUTSTANDING";
+    remarks = "PASSED";
+  } else if (averageGrade >= 85 && averageGrade <= 89) {
+    descriptor = "VERY SATISFACTORY";
+    remarks = "PASSED";
+  } else if (averageGrade >= 80 && averageGrade <= 84) {
+    descriptor = "SATISFACTORY";
+    remarks = "PASSED";
+  } else if (averageGrade >= 75 && averageGrade <= 79) {
+    descriptor = "FAIRLY SATISFACTORY";
+    remarks = "PASSED";
+  } else if(averageGrade >= 0 && averageGrade <= 74){
+    descriptor = "DID NOT MEET EXPECTATIONS";
+    remarks = "FAILED";
+  } else {
+    alert('please Enter Your marks, Then click Calculate button');
+  }
+  // Display the result to the user , &nbsp; is used to add spaces
+  document.getElementById("result").innerHTML = 
+  `<span>Total Grade:</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${averageGrade.toFixed(2)}
+  <br><br><span>Descriptor:</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${descriptor}
+  <br><br> <span>Remarks:</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    ${remarks}`;
+}
---
+++ js/reset.js
@@ -0,0 +1,10 @@
+function resetButton() {
+    document.getElementById("subject1").value = "";
+    document.getElementById("subject2").value = "";
+    document.getElementById("subject3").value = "";
+    document.getElementById("subject4").value = "";
+    document.getElementById("subject5").value = "";
+    document.getElementById("subject6").value = "";
+    document.getElementById("subject7").value = "";
+    document.getElementById("result").innerHTML = "";
+  }
